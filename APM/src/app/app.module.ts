2e7193b8ffa2ce7ab2a11dd576bc1f5ca00bdda0
Reginald Arnedo <reginald.arnedo@gmail.com>
2023-03-27 10:38:34 -0400

    Next sections of course

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
@@ -13,7 +15,7 @@ import { StarComponent } from './shared/star.component';
     ConvertToSpacesPipe,
     StarComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
